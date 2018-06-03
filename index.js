3ebdbd8cb5cf78e25ab00bd6c6fc444f23d24a1d
mathhulk <no-reply@mathhulk.com>
2018-06-02 20:44:15 -0700

    Overhaul from previous versions

--- index.js
+++ index.js
@@ -1,56 +1,93 @@
 //
 //	VARIABLES
 //
-const axios = require("axios");
+const request = require("request");
 const path = require("path");
 const Express = require("express");
 const express = Express();
 
-const READ = "https://whats-th.is/public-cdn-domains.txt", TEST = "1e661d";
+const DOMAINS = "https://whats-th.is/public-cdn-domains.txt", FILE = "1e661d";
+const SERVICES = {
+	api: {
+		uri: "https://api.awau.moe/health",
+		name: "API",
+		method: "GET",
+		code: 401
+	},
+	gitlab: {
+		uri: "https://owo.codes/help",
+		name: "GitLab",
+		method: "HEAD",
+		code: 200
+	},
+	mastadon: {
+		uri: "https://uwu.social/about",
+		name: "Mastadon",
+		method: "HEAD",
+		code: 200
+	},
+	matrix: {
+		uri: "https://yuri.im/_matrix/federation/v1",
+		name: "Matrix",
+		method: "HEAD",
+		code: 400
+	}
+};
 
-var status = {};
+var status = {services: {}, domains: {}}, timeout;
 
 //
 //	FUNCTIONS
 //
-function validateDomain(domain) {
-	return !(domain === "" || domain.substring(0, 1) === "#" || domain.includes(":"));
+function getService(uri, name, method, code) {
+	request(uri, method, function(error, response, data) {
+		if(error) status.domains[cleanDomain(domain, true)] = false;
+		else status.services[name] = response && response.statusCode === code;
+	});
 }
 
-async function getDomains(uri, file) {
-	var domains;
+function getServices(list) {
+	for(var service in list) getService(list[service].uri, list[service].name, list[service].method, list[service].code);
+}
+
+function getDomain(domain, file) {
+	request("https://" + domain + "/" + file, function(error, response, data) {
+		if(error) status.domains[cleanDomain(domain, true)] = false;
+		else status.domains[cleanDomain(domain, true)] = response && response.statusCode === 200;
+	});
+}
+
+function getDomains(uri, file) {
+	request(uri, function(error, response, data) {
+		if(error) console.log(error);
+		data = data.split("\n");
+		for(var domain in status.domains) if(!data.includes(domain)) delete status.domains[domain];
+		data.forEach(function(line, index) {
+			if(isDomain(line)) getDomain(cleanDomain(line, false), file);
+		});
+	});
+}
+
+function isDomain(line) {
+	return !(line === "" || line.substring(0, 1) === "#" || line.includes(":"));
+}
+
+function cleanDomain(domain, reverse) {
+	if(reverse) return domain.replace("wildcard.", "*.");
+	return domain.replace("*.", "wildcard.");
+}
+
+function getStatus(list, uri, file) {
+	getDomains(uri, file);
+	getServices(list);
 	
-	try {
-		domains = await axios.get(uri);
-		
-		domains = domains.data.split("\n");
-	} catch(error) {
-		console.log(error);
-	}
-	
-	for(var i = 0; i < domains.length; i++) {
-		if(validateDomain(domains[i])) status[domains[i]] = "unknown";
-	}
-	
-	for(var i = 0; i < domains.length; i++) {
-		if(validateDomain(domains[i])) {
-			try {
-				await axios.get("https://" + domains[i].replace("*", "example") + "/" + file);
-				
-				status[domains[i]] = "good";
-			} catch (error) {
-				status[domains[i]] = "bad";
-			}
-		}
-	}
-	
-	setTimeout(getDomains, 1000 * 60 * 5, uri, file);
+	timeout = setTimeout(getStatus, 5000, list, uri, file);
 }
 
 //
 //	REQUEST
 //
-getDomains(READ, TEST);
+getStatus(SERVICES, DOMAINS, FILE);
 
 express.use(Express.static(path.join(__dirname, "public")));
 
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,27 +1,26 @@
 //
-//	VARIABLES
-//
-var application = new Vue({
-	el: ".application",
-	data: {
-		status: {}
-	}
-});
-
-//
 //	EVENTS
 //
 $(document).ready(function() {
-	$.getJSON("/status", function(data) {
-		application.status = data;
+	$.get("/templates/status.txt", function(template) {
+		$.getJSON("/status", function(status) {
+			$.each(status.domains, function(index, value) {
+				$(".domains").append(template.replace("{{ domain }}", index).replace("{{ status }}", value));
+			});
+			$.each(status.services, function(index, value) {
+				$(".services").append(template.replace("{{ domain }}", index).replace("{{ status }}", value));
+			});
+		});
 	});
 	
 	$(document).on("click", ".sort .select", function() {
 		if($(this).attr("data-sort") === "all") {
-			$(".status").show();
+			$(".status, h2, .services, .domains").show();
 		} else {
 			$(".status").hide();
-			$(".status." + $(this).attr("data-sort")).show();
+			$(".services, .domains, h2:first, h2:last, .status." + $(this).attr("data-sort")).show();
+			if($(".services").children(":visible").length === 0) $("h2:first, .services").hide();
+			if($(".domains").children(":visible").length === 0) $("h2:last, .domains").hide();
 		}
 	});
 });
