4f1b35b2d8e50d659783264ff68a9fdf6c321fbe
mathhulk <no-reply@mathhulk.com>
2018-09-10 15:51:50 -0700

    New design and changes

---
+++ main.js
@@ -0,0 +1,84 @@
+// VARIABLES
+const request = require("request");
+const fs = require("fs");
+const Express = require("express");
+const express = Express( );
+
+const configuration = JSON.parse(fs.readFileSync("storage/configuration.json"), "utf8");
+var templates = { };
+var status = { };
+
+// FUNCTIONS
+function getDomains( ) {
+	request("https://whats-th.is/public-cdn-domains.txt", function(error, response, data) {
+		if(error) console.log(error);
+		
+		status.domains = { };
+		
+		data.split("\n").forEach(function(value, index) {
+			if(isDomain(value)) getDomain(parseDomain(value, false));
+		});
+	});	
+}
+
+function getDomain(domain) {
+	request("https://" + domain, function(error, response, data) {
+		status.domains[parseDomain(domain, true)] = { };
+		
+		if(error) status.domains[parseDomain(domain, true)].online = false;
+		else status.domains[parseDomain(domain, true)].online = response && response.statusCode === 200;
+	});
+}
+
+function isDomain(string) {
+	return !(string === "" || string.substring(0, 1) === "#" || string.includes(":"));
+}
+
+function parseDomain(domain, reverse) {
+	return reverse ? domain.replace("wildcard.", "*.") : domain.replace("*.", "wildcard.");
+}
+
+function getService(service) {
+	request(configuration.services[service].uri, configuration.services[service].method, function(error, response, data) {
+		status.services[service] = {href: configuration.services[service].href, description: configuration.services[service].description};
+		
+		if(error) status.services[service].online = false;
+		else status.services[service].online = response && response.statusCode === configuration.services[service].code;
+	});
+}
+
+function getServices( ) {
+	status.services = { };
+	
+	for(let service in configuration.services) {
+		getService(service, configuration.services[service].uri, configuration.services[service].method, configuration.services[service].code);
+	}
+}
+
+function loadStatus( ) {
+	getDomains( );
+	getServices( );
+}
+
+// LOAD
+loadStatus( );
+setInterval(function( ) {
+	fs.writeFile("public/status.json", JSON.stringify(status), function(error) {
+		if(error) console.log(error);
+	});
+	
+	loadStatus( );
+}, 5000);
+
+// EXPRESS
+express.use(Express.static(__dirname + "/public"));
+
+express.get("/", (request, response) => {
+	response.sendFile(__dirname + "/public/index.html");
+});
+
+express.get("/status", (request, response) => {
+	response.sendFile(__dirname + "/public/status.json");
+});
+
+express.listen(8999);
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,48 +1,24 @@
-/*
- *	FUNCTIONS
- */
-function showStatus() {
-	$(".status, h2:not(:eq(2)), .services, .domains").show();
-	$("h2:eq(2)").hide();
+// FUNCTIONS
+function parse(template, data) {
+	$.each(data, function(index, value) {
+		template = template.replace(new RegExp(index, "g"), value);
+	});
+	return template;
 }
 
-function toggleTitle() {
-	if($(".services").children(":visible").length === 0) $("h2:first, .services").hide();
-	if($(".domains").children(":visible").length === 0) $("h2:eq(1), .domains").hide();
-	if($("h2:visible").length === 0) $("h2:eq(2)").show();
-}
-
-/*
- *	EVENTS
- */
-$(document).ready(function() {
-	$(".sort").show();
-	$("h2:first-of-type").css("padding-top", "30px");
-	
-	$(document).on("focus", ".sort .search", function() {
-		$(this).removeAttr("placeholder");
-	});
-	
-	$(document).on("blur", ".sort .search", function() {
-		$(this).attr("placeholder", "Find");
-	});
-	
-	$(document).on("click", ".sort .select", function() {
-		showStatus();
-		if($(this).attr("data-sort") !== "all") {
-			$(".status:not(." + $(this).attr("data-sort") + ")").hide();
-			toggleTitle();
-		}
-	});
-	
-	$(document).on("keyup", ".sort .search", function() {
-		var search = $(this).val().toLowerCase();
-		showStatus();
-		if(search !== "") {
-			$(".status").each(function() {
-				if(!$(this).text().toLowerCase().includes(search)) $(this).hide();
+// EVENTS
+$(document).ready(function( ) {
+	$.get("status", function(data) {	
+		$.get("templates/service.txt", function(template) {
+			$.each(data.services, function(index, value) {
+				$("#services .row").append(parse(template, {"{{ status }}": value.online ? "online" : "offline", "{{ name }}": index, "{{ description }}": value.description, "{{ href }}": value.href}));
 			});
-			toggleTitle();
-		}
+		});
+		
+		$.get("templates/domain.txt", function(template) {
+			$.each(data.domains, function(index, value) {
+				$("#domains .row").append(parse(template, {"{{ status }}": value.online ? "online" : "offline", "{{ name }}": index}));
+			});
+		});
 	});
 });
