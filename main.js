b44ed19421feb48e0a882076530ff616598d5eca
mathhulk <no-reply@mathhulk.com>
2018-11-07 17:04:19 -0800

    Organization and small updates
    Updated dependencies, organized JavaScript files, removed unnecessary comments, changed file structure, changed description

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-// VARIABLES
 const request = require("request");
-const fs = require("fs");
+const system = require("fs");
+
 const Express = require("express");
 const express = Express( );
 
-const configuration = JSON.parse(fs.readFileSync("storage/configuration.json"), "utf8");
-var status = { };
+const configuration = JSON.parse(system.readFileSync("storage/configuration.json"), "utf8");
 
-// FUNCTIONS
+let material = { };
+
 function getDomains( ) {
 	request("https://whats-th.is/public-cdn-domains.txt", function(error, response, data) {
 		if(error) console.log(error);
 		
-		status.domains = { };
+		material.domains = { };
 		
 		data.split("\n").forEach(function(value, index) {
 			if(isDomain(value)) getDomain(parseDomain(value, false));
@@ -22,10 +22,10 @@ function getDomains( ) {
 
 function getDomain(domain) {
 	request("https://" + domain, function(error, response, data) {
-		status.domains[parseDomain(domain, true)] = { };
+		material.domains[parseDomain(domain, true)] = { };
 		
-		if(error) status.domains[parseDomain(domain, true)].online = false;
-		else status.domains[parseDomain(domain, true)].online = response && response.statusCode === 200;
+		if(error) material.domains[parseDomain(domain, true)].online = false;
+		else material.domains[parseDomain(domain, true)].online = response && response.statusCode === 200;
 	});
 }
 
@@ -39,37 +39,28 @@ function parseDomain(domain, reverse) {
 
 function getService(service) {
 	request(configuration.services[service].uri, configuration.services[service].method, function(error, response, data) {
-		status.services[service] = {href: configuration.services[service].href, description: configuration.services[service].description};
+		material.services[service] = {href: configuration.services[service].href, description: configuration.services[service].description};
 		
-		if(error) status.services[service].online = false;
-		else status.services[service].online = response && response.statusCode === configuration.services[service].code;
+		if(error) material.services[service].online = false;
+		else material.services[service].online = response && response.statusCode === configuration.services[service].code;
 	});
 }
 
 function getServices( ) {
-	status.services = { };
+	material.services = { };
 	
-	for(let service in configuration.services) {
-		getService(service, configuration.services[service].uri, configuration.services[service].method, configuration.services[service].code);
-	}
+	for(let service in configuration.services) getService(service, configuration.services[service].uri, configuration.services[service].method, configuration.services[service].code);
 }
 
-function loadStatus( ) {
+function getMaterial( ) {
 	getDomains( );
 	getServices( );
+	
+	setTimeout(getMaterial, 60 * 1000 * 10);
 }
 
-// LOAD
-loadStatus( );
-setInterval(function( ) {
-	fs.writeFile("public/status.json", JSON.stringify(status), function(error) {
-		if(error) console.log(error);
-	});
-	
-	loadStatus( );
-}, 60 * 1000 * 10);
+getMaterial( );
 
-// EXPRESS
 express.use(Express.static(__dirname + "/public"));
 
 express.get("/", (request, response) => {
@@ -77,7 +68,9 @@ express.get("/", (request, response) => {
 });
 
 express.get("/status", (request, response) => {
-	response.sendFile(__dirname + "/public/status.json");
+	response.json(material);
 });
 
 express.listen(8999);
+
+console.log("Listening on port 8999.");
--- public/resources/javascript/script.js
+++ public/resources/javascript/script.js
@@ -1,11 +1,12 @@
-// VARIABLES
-var template, domains, search = { };
+let template = { };
+let domains = { };
+let search = { };
 
-// FUNCTIONS
-function parse(template, replace) {
+function replace(template, replace) {
 	$.each(replace, function(index, value) {
 		template = template.replace(new RegExp(index, "g"), value);
 	});
+	
 	return template;
 }
 
@@ -14,26 +15,34 @@ function sort( ) {
 	
 	$.each(domains, function(index, value) {
 		if(validate(index, value)) {
-			$("#domains .row").append(parse(template, {"{{ status }}": value.online ? "online" : "offline", "{{ name }}": index}));
+			$("#domains .row").append(replace(template, {"{{ status }}": value.online ? "online" : "offline", "{{ name }}": index}));
 		}
 	});
 }
 
 function validate(domain, data) {
-	if(typeof(search.online) === "boolean" && search.online !== data.online) return false;
-	if(search.term && !domain.includes(search.term)) return false;
+	if(typeof(search.online) === "boolean" && search.online !== data.online) {
+		return false;
+	}
+	
+	if(search.term && !domain.includes(search.term)) {
+		return false;
+	}
+	
 	return true;
 }
 
-// EVENTS
 $(document).ready(function( ) {
+	
 	$(document).on("click", ".dropdown-item", function( ) {
 		if($(this).hasClass("active")) {
 			$(this).removeClass("active");
+			
 			delete search.online;
 		} else {
 			$(this).parent( ).children( ).removeClass("active");
 			$(this).addClass("active");
+			
 			search.online = $(this).attr("data-status") === "true";
 		}
 		
@@ -49,18 +58,19 @@ $(document).ready(function( ) {
 	$.get("status", function(data) {
 		domains = data.domains;
 		
-		$.get("templates/service.txt", function(service) {
+		$.get("resources/templates/service.tpl", function(service) {
 			$("#services .row").empty( );
 			
 			$.each(data.services, function(index, value) {
-				$("#services .row").append(parse(service, {"{{ status }}": value.online ? "online" : "offline", "{{ name }}": index, "{{ description }}": value.description, "{{ href }}": value.href}));
+				$("#services .row").append(replace(service, {"{{ status }}": value.online ? "online" : "offline", "{{ name }}": index, "{{ description }}": value.description, "{{ href }}": value.href}));
 			});
 		});
 		
-		$.get("templates/domain.txt", function(domain) {
+		$.get("resources/templates/domain.tpl", function(domain) {
 			template = domain;
 			
 			sort( );
 		});
 	});
+	
 });
